d60f4d75f2fac3757f95e6031ea9604b57df751a
zerbinidamata <rafael.zerbinia@gmail.com>
2022-12-06 10:09:30 -0300

    feat(interceptors)

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerService } from '@/infrastructure/logger/logger.service';
 import { AllExceptionFilter } from '@/infrastructure/common/filter/exception.filter';
+import { LoggingInterceptor } from '@/infrastructure/common/interceptors/logger.interceptor';
+import { ResponseInterceptor } from '@/infrastructure/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
